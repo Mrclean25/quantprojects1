e127c8254a44dbfac20d74c4363f48f681686bcf
Mrclean25 <rasheed.mr.clean@gmail.com>
2026-01-02 12:29:54 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from src.data_loader import load_btc_data
-from src.features import moving_average, volatility
+from src.features import create_features
+from src.ml_model import prepare_data, train_and_test
 
-df = load_btc_data()
+df = load_btc_data(start="2018-01-01")
 
-df["ma"] = moving_average(df["close"], 5)
-df["vol"] = volatility(df["close"], 5)
+df = create_features(df)
 
-print(df.tail())
+X, y = prepare_data(df)
+
+model, train_acc, test_acc = train_and_test(X, y)
+
+print("Train Accuracy:", round(train_acc, 3))
+print("Test Accuracy:", round(test_acc, 3))
