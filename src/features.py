0df991a2ea6720cdcacd89d81bc6af5f04d81cc7
Mrclean25 <rasheed.mr.clean@gmail.com>
2026-01-02 12:36:46 -0400

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,4 +1,4 @@
-import numpy as np
+import pandas as pd
 
 def moving_average(series, window):
     return series.rolling(window).mean()
@@ -7,24 +7,24 @@ def rsi(series, period=14):
     delta = series.diff()
     gain = delta.clip(lower=0)
     loss = -delta.clip(upper=0)
-
     avg_gain = gain.rolling(period).mean()
     avg_loss = loss.rolling(period).mean()
-
     rs = avg_gain / avg_loss
     return 100 - (100 / (1 + rs))
 
 def volatility(series, window=5):
     return series.pct_change().rolling(window).std()
 
-def create_features(df):
+def create_features(df, ma_short_window=5, ma_long_window=10, rsi_period=5, vol_window=5):
+ 
+    #Add features to dataframe for ML model.
+    
     df = df.copy()
-
     df["return"] = df["close"].pct_change()
-    df["ma_short"] = moving_average(df["close"], 5)
-    df["ma_long"] = moving_average(df["close"], 10)
-    df["rsi"] = rsi(df["close"], 5)
-    df["volatility"] = volatility(df["close"], 5)
-
+    df["ma_short"] = moving_average(df["close"], ma_short_window)
+    df["ma_long"] = moving_average(df["close"], ma_long_window)
+    df["rsi"] = rsi(df["close"], rsi_period)
+    df["volatility"] = volatility(df["close"], vol_window)
     return df.dropna()
+
 
