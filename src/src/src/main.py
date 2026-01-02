7f484016072a7e06b5b6665d137eee995c20ecfd
Mrclean25 <rasheed.mr.clean@gmail.com>
2026-01-02 12:21:08 -0400

    Delete src/src/src/main.py

--- src/src/src/main.py
+++
@@ -1,9 +0,0 @@
-from src.data_loader import load_btc_data
-from src.features import moving_average, volatility
-
-df = load_btc_data()
-
-df["ma"] = moving_average(df["close"], 5)
-df["vol"] = volatility(df["close"], 5)
-
-print(df.tail())
