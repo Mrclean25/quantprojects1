2a1f3bec654f9cb5c8ac1e740485143ac0e9edd3
Mrclean25 <rasheed.mr.clean@gmail.com>
2026-01-02 12:31:32 -0400

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,15 @@ import yfinance as yf
 import pandas as pd
 
 def load_btc_data(start="2018-01-01"):
-    btc = yf.download("BTC-USD", start=start, progress=False)
+    btc = yf.download(
+        "BTC-USD",
+        start=start,
+        progress=False
+    )
+
     df = btc[["Close"]].reset_index()
     df.columns = ["date", "close"]
-    return df.dropna()
+    df = df.dropna()
+
+    return df
+
