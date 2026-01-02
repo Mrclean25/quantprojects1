b8ec816f42128595377e46ce5cd5f76d115af2b9
Mrclean25 <rasheed.mr.clean@gmail.com>
2026-01-02 13:01:52 -0400

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,9 +2,9 @@ import yfinance as yf
 import pandas as pd
 
 def load_crypto_data(symbol="BTC-USD", start="2024-06-01"):
-    
-    #Download historical price data from Yahoo Finance.
-    
+    """
+    Download historical price data from Yahoo Finance.
+    """
     df = yf.download(symbol, start=start, progress=False)[["Close"]].reset_index()
     df.columns = ["date", "close"]
     if df.empty:
